44676670d628480bb43c1064c5f9e3d2774b94a0
Ameya Patil <amepatil@microsoft.com>
2021-02-02 18:32:16 +0000

    Updating MSAL framework checksum & url [skip ci]

--- Package.swift
+++ Package.swift
@@ -13,6 +13,6 @@ let package = Package(
           targets: ["MSAL"]),
   ],
   targets: [
-      .binaryTarget(name: "MSAL", url: "https://github.com/ameyapat/pipeline-test/releases/download/22/MSAL.zip", checksum: "abcdefabcdef1234567890abcdef1234567890abcdef1234567890abcdef1234")
+      .binaryTarget(name: "MSAL", url: "https://github.com/ameyapat/pipeline-test/releases/download/23/MSAL.zip", checksum: "abcdefabcdef1234567890abcdef1234567890abcdef1234567890abcdef1234")
   ]
 )
